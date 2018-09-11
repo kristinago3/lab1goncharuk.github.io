60f8e96b0d729e1bf7285bd87effbefb95a4dba3
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-09-11 21:33:20 +0300

    Add files via upload

---
+++ 111.js
@@ -0,0 +1,36 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+
+ 
+function g()
+{
+if($("sl1").selectedIndex==0)
+
+{alert('ПРИВЕТ,МИР!');$("b4").style.backgroundColor="green";}
+
+if($("sl1").selectedIndex==1)
+{alert('ПРИВЕТ,МИР!');$("b4").style.color="blue";}
+
+}
+
+function f()
+
+{alert('ПРИВЕТ,МИР!');$("pic1").src="сова.jpg"}
+
+
+
+
+
+
+
+
