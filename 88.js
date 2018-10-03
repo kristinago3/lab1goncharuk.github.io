aa9c6d81f1adacb96ea18e7252039a839da092c4
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-10-03 10:10:25 +0300

    Update 88.js

--- 88.js
+++ 88.js
@@ -12,14 +12,15 @@ return elements; }
 
 function f1()
 {       x=parseFloat($('tx1').value);
-	N=parseFloat($('tx2').value);
+	N=parseInt($('tx2').value);
 S=x;
 p1=1;
 p2=1;
 for(i=1;i<=N;i++)
 {p1=p1*(2*i+1);
 p2=p2*(-1);
-S=S+((Math.pow(x,p1)*p2*Math.pow(p2,i))/p1);
+ S=S+p1+p2;
+//S=S+((Math.pow(x,p1)*p2*Math.pow(p2,i))/p1);
 }
 $('tx3').value=''+S;
 }
