8b8c805baf0be0abc24c8b7b20d5fcfc0628690f
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-10-03 09:47:46 +0300

    Update 99.js

--- 99.js
+++ 99.js
@@ -14,14 +14,14 @@ return elements; }
 function f1()
 {N=parseFloat($('tt1').value);
  K=parseFloat($('tt2').value);
- Sum=1;
+ S=1;
  
                      for(i=2;i<=N;i++) 
 		       {a=i;
                            for(j=2;j<=K;j++)
                        {a=a*i;
-                        Sum=Sum+a;}
-		  	$('tt3').value=Sum;
+                        S=S+a;}
+		  	$('tt3').value=S;
                                 
 		             }
 
