b35de9f01dd0e648a9abe379af6794cc987389e3
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-10-07 14:36:23 +0300

    Update k88.js

--- k88.js
+++ k88.js
@@ -11,9 +11,9 @@ elements.push(element);
 return elements; }
 
 function f1()
-{       x=parseFloat($('tx1').value);
-	N=parseInt($('tx2').value);
- S=x;
+{x=parseFloat($('tx1').value);
+N=parseInt($('tx2').value);
+ S=1;
  p1=1;
  p2=1;
 for(i=1;i<=N;i++)
@@ -22,5 +22,5 @@ p2=p2*(-1);
 S=S+p1+p2;
 //S=S+((Math.pow(x,p1)*p2*Math.pow(p2,i))/p1);
 }
-$('tx3').value=S;
+$('tx3').value=''+S;
 }
