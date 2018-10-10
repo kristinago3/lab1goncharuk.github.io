b6fad4e01c6d711702eb3d9074f220b1e5a4c2bf
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-10-10 11:36:41 +0300

    Add files via upload

---
+++ k10.js
@@ -0,0 +1,26 @@
+﻿/*function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+*/
+
+function f1()
+{x=parseFloat(document.getElementById('tx1').value);
+N=parseInt(document.getElementById('tx2').value);
+ S=x;
+ p1=1;
+ p2=1;
+for(i=1;i<=N;i++)
+{p1=p1*(2*i+1);
+p2=p2*(-1);
+S=S+((Math.pow(x,p1)*p2*Math.pow(p2,i))/p1);
+}
+document.getElementById('tx3').value=''+S;
+}
