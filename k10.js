ea1ede78ac52157e6e068690f789e0b4621b1c9d
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-10-10 11:57:05 +0300

    Update k10.js

--- k10.js
+++ k10.js
@@ -12,15 +12,12 @@ return elements; }
 */
 
 function f1()
-{x=parseFloat(document.getElementById('tx1').value);
-N=parseInt(document.getElementById('tx2').value);
- S=x;
- p1=1;
- p2=1;
-for(i=1;i<=N;i++)
-{p1=p1*(2*i+1);
-p2=p2*(-1);
-S=S+((Math.pow(x,p1)*p2*Math.pow(p2,i))/p1);
-}
-document.getElementById('tx3').value=''+S;
+{N=parseInt(document.getElementById('tx1').value);
+ i=1;
+ while(i<=8785856)
+  i++;
+ {
+            fact=fact*i;
+        }
+ document.getElementById('tx2').value=i;
 }
