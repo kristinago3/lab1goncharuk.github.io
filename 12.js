3e24ccb03d3b664dfca08506dcef0e22c3805bfc
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-10-31 12:29:44 +0200

    Update 12.js

--- 12.js
+++ 12.js
@@ -15,10 +15,10 @@ function f(x)
 {var x=parseFloat(document.getElementById('In').value);
  var a = '1234567890';
  fff=1;
-   for (var i = 0; i < str.length; i++)
+   for (var i = 0; i < x.length; i++)
    {
-      x=str.charAt(i);
-    if(x.indexOff(a)<0)
+      b=x.charAt(i);
+    if(b.indexOff(a)<0)
      fff=0
    }
  if(fff)
