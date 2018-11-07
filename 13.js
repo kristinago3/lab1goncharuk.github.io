3ca8a7fe058b7ce4fefedbfdd305e0c586187fb6
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-11-07 11:34:32 +0200

    Add files via upload

---
+++ 13.js
@@ -0,0 +1,15 @@
+﻿/*function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+*/
+var t;
+function f()
+{document.getElementById('b4').style.left=(parseInt(document.getElementById('b4').style.left+1)+'px');}
