dc60d382a1763732e358cacaad322fb4272bd68e
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-11-28 09:44:18 +0200

    Update 13.js

--- 13.js
+++ 13.js
@@ -10,6 +10,29 @@ elements.push(element);
 } 
 return elements; } 
 */
-var t;
-function f()
-{document.getElementById('b4').style.left=(parseInt(document.getElementById('b4').style.left+1)+'px');}
+var curPosX = 0;
+var curPosY = 0;
+var interval;
+var n = 10; // На сколько двигать за раз
+var width = document.documentElement.clientWidth; // Ширина экрана
+var height = document.documentElement.clientHeight; // Высота экрана
+var imgWidth = 100; // Ширина картинки
+var imgHeight = 100; // Высота картинки
+var img1 = document.getElementById("img1");
+
+function move() {
+  img1.style.left = (curPosX += n) + "px";
+  img1.style.top = (curPosY += n) + "px";
+  if ((curPosX == (width - imgWidth)) || (curPosY == (height - imgHeight))) {
+    clearInterval(interval);
+  }
+}
+interval = setInterval(move, 100);
+#img1 {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100px;
+  height: 100px;
+  background-color: #000000;
+}
