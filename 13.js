f9fe2371f91a25000ddb727ede982afcb97a6d65
kristinago3 <39299838+kristinago3@users.noreply.github.com>
2018-12-04 22:43:10 +0200

    Update 13.js

--- 13.js
+++ 13.js
@@ -10,37 +10,28 @@ elements.push(element);
 } 
 return elements; } 
 */
-﻿function $() 
-{ var elements = new Array(); 
-for (var i = 0; i < arguments.length; i++) 
-{ var element = arguments[i]; 
-if (typeof element == 'string') 
-element = document.getElementById(element); 
-if (arguments.length == 1) 
-return element; 
-elements.push(element); 
-} 
-return elements; }
 
 var t;
 var r1= 0;
 var g1= 255;
-var b4= 0;
+var b1= 0;
 function f()
-	{	var e = document.getElementById('b4');
-		e.style.right=(parseInt(e.style.right)-30)+'px';
-		if(parseInt(e.style.right) <=0)
+	{	
+		var e = document.getElementById('b1');
+		e.style.left=(parseInt(e.style.left)-30)+'px';
+		if(parseInt(e.style.left) <=0)
 			{
 				s();
 			} 
 		g1 = g1-6;
-		
+		color ='rgb('+r1+', '+g1+', '+b1+')';
+		e.style.backgroundColor= color;
 	}
 
 function r()
 	{
-		var x = document.getElementById('b4');
-		x.style.right = "900px";
+		var x = document.getElementById('b1');
+		x.style.left = "900px";
 		t = setInterval('f()',100);
 	}
 function s()
